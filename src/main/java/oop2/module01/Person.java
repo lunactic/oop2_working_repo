73b983169756f6b0703f0bd92a85fd5bc41d9b14
Lucifer Uchiha <rowla101@gmail.com>
2021-03-01 09:59:38 +0100

    made branch ready for pull request

--- src/main/java/oop2/module01/Person.java
+++ src/main/java/oop2/module01/Person.java
@@ -1,18 +1,15 @@
 package oop2.module01;
 
-import java.util.Arrays;
-
 /**
  * @author Dieter Holz
  */
 public class Person {
-    private int age;
+    private int    age;
     private double weight;
     private double height;
-    private Person[] friends = new Person[0];
 
     public Person(int age, double weight, double height) {
-        this.age = age;
+        this.age    = age;
         this.weight = weight;
         this.height = height;
     }
@@ -45,28 +42,5 @@ public class Person {
 
     public void setHeight(double height) {
         this.height = height;
-    }
-
-    public Person[] getFriends() {
-        return friends;
-    }
-
-    public int getNumberOfFriends() {
-        return friends.length;
-    }
-
-    public void addFriend(Person friend) {
-        // Extend array by one
-        friends = Arrays.copyOf(friends, friends.length + 1);
-        // Add friend
-        friends[friends.length - 1] = friend;
-    }
-
-    public boolean hasFriend(Person friend) {
-        for (Person p: friends){
-            if (p.equals(friend))
-                return true;
-        }
-        return false;
     }
 }
